71fc9c34c913acbb02263faa4bab4230711cbfac
Pavel Shpidonov <pshpidonov@mail.ru>
2025-09-07 14:33:33 +0300

    added url for deploy

--- src/env/constants.ts
+++ src/env/constants.ts
@@ -1,1 +1,2 @@
-export const API_URL: string = 'http://localhost:5000/api';
+// export const API_URL: string = 'http://localhost:5000/api';
+export const API_URL: string = 'http://212.8.228.25:5000/api';
